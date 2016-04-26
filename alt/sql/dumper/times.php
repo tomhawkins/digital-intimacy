684cce71e491a06062c26c13ec6c9fd619176398
Tom Hawkins <tomhawkins94@outlook.com>
2016-04-26 21:06:05 +0100

    still need to flatten array perhaps?

--- alt/sql/dumper/times.php
+++ alt/sql/dumper/times.php
@@ -17,43 +17,64 @@ $times = array  ("00:00:00 - 04:00:00"  => array (),
                 );
 
  $timeresult = $retrieval->get_post_times();
+$array = array_values($timeresult);
+
+//function array_flatten($array) { 
+//  if (!is_array($array)) { 
+//    return FALSE; 
+//  } 
+//  $result = array(); 
+//  foreach ($array as $key => $value) { 
+//    if (is_array($value)) { 
+//      $result = array_merge($result, array_flatten($value)); 
+//    } 
+//    else { 
+//      $result[$key] = $value; 
+//    } 
+//  } 
+//  return $result;
+//  var_dump($result);
+//  print_r($result);
+//  
+//} 
+
  //print_r($locations);
 
 
-// for($i=0; $i<count($timeresult); $i++){
-//     
-//     if($retrieval->check_time("00:00:00", "04:00:00", $timeresult[4])){
-//    $result = print "Range 1 ";
-//} 
-//
-//else if($retrieval->check_time("04:00:01", "08:00:00", $timeresult[4])){
-//    $result = print "Range 2 ";
-//} 
-//
-//else if($retrieval->check_time("08:00:01", "12:00:00", $timeresult[4])){
-//    $result = print "Range 3 ";
-//} 
-//
-//else if($retrieval->check_time("12:00:01", "16:00:00", $timeresult[4])){
-//    $result = print "Range 4 ";
-//} 
-//
-//else if($retrieval->check_time("16:00:01", "20:00:00", $timeresult[4])){
-//    $result = print "Range 5 ";
-//} 
-//
-//else {
-//    $result = print "Range 6 ";
-//}
-//
-// }
+ for($i=0; $i<count($timeresult); $i++){
+     
+     if($retrieval->check_time("00:00:00", "04:00:00", $array[12])){
+    $result = print "Range 1 ";
+} 
 
-print_r(array_values($timeresult));
+else if($retrieval->check_time("04:00:01", "08:00:00", $array[12])){
+    $result = print "Range 2 ";
+} 
+
+else if($retrieval->check_time("08:00:01", "12:00:00", $array[12])){
+    $result = print "Range 3 ";
+} 
+
+else if($retrieval->check_time("12:00:01", "16:00:00", $array[12])){
+    $result = print "Range 4 ";
+} 
+
+else if($retrieval->check_time("16:00:01", "20:00:00", $array[12])){
+    $result = print "Range 5 ";
+} 
+
+else {
+    $result = print "Range 6 ";
+}
+
+ }
+
+print_r($result);
+
 //$result = $timeresult;
 
  //         $encoded = json_encode($everything);
  // 		print_r($encoded);
 //        var_dump($result);
 // 		return $result;
-
 ?>
